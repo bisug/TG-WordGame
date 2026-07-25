783946f7a399351121aef9a2db84b9b2c217841f
Bisu Ghalan <bisu.ghlan@gmail.com>
2026-07-25 17:34:21 +0545

    fix: correct username link formatting by removing unnecessary HTML escaping

--- src/util/format-leaderboard-message.ts
+++ src/util/format-leaderboard-message.ts
@@ -10,7 +10,7 @@ export function formatLeaderboardMessage(
   const blocks = data.reduce((acc, entry, index) => {
     const rank = rankIcons[index] ?? "🔅";
 
-    let usernameLink = escapeHtmlEntities(entry.name);
+    let usernameLink = entry.name;
     if (entry.username) {
       usernameLink = `<a href="t.me/${entry.username}">${escapeHtmlEntities(
         entry.name,
